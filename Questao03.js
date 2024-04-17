4f9b91a6e9e820e31aee1d72c6987078ee892147
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-17 17:13:58 -0400

    Questão 03 concluida

---
+++ Questao03.js
@@ -0,0 +1,16 @@
+/* 
+Questão 03 – Crie um algoritmo usando o método forEach() para encontrar o 
+maior número em um array de números?
+*/
+
+let array = [1,2,3,4,50];
+
+let x = 0
+
+array.forEach(function(num){
+    if (num > x){
+        x = num
+    }
+})
+
+console.log(b)
