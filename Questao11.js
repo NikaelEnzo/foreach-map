73d58cfc25a2e6211981567750e31d9d9b51dddf
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 14:56:26 -0400

    Questão 11 concluida

---
+++ Questao11.js
@@ -0,0 +1,10 @@
+/* 
+Questão 11 - map() sempre retorna um novo array com o mesmo comprimento 
+que o array original?   
+
+sim, map retornara o mesmo número de elementos do array original para o array novo
+
+
+
+
+*/
