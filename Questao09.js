bc157e66bdb0721727238aa8b4804e2b4368d4a7
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 14:39:06 -0400

    Questão 09 concluida

---
+++ Questao09.js
@@ -0,0 +1,12 @@
+/* 
+Questão 09 - Qual é a diferença fundamental entre map() e forEach()?  
+
+map é usado para criar um novo array com os resultados de uma função de callback
+
+forEach é usado para imprimir os valores de um array especifico sem altera-los
+
+
+
+
+
+*/
