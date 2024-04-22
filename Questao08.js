7d355ed9ade7058c9352ca69d476e3cba3763b52
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 14:35:02 -0400

    Questão 08 concluida

---
+++ Questao08.js
@@ -0,0 +1,20 @@
+/* 
+Questão 08 – Crie um algoritmo usando o método forEach() para calcular a 
+média de todos os números em um array?  
+*/
+
+let array = [5,3,3,4,5];
+
+let contArray = array.length;
+
+let soma = 0;
+
+let media = 0;
+
+array.forEach(function(num){
+    soma += num;
+    media = soma / contArray;
+})
+
+console.log(soma)
+console.log(media)
