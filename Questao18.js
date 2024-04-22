aab25985b972f5454d5e2640af3af248fbc02db5
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 16:22:44 -0400

    Questão 18 concluida

---
+++ Questao18.js
@@ -0,0 +1,8 @@
+/* 
+Questão 18 - O que acontece se você chamar map() em um array vazio?
+
+ele não irá conseguir adicionar um valo na array nova, porque a array original contém nada, fazendo com que não funcione
+
+
+
+*/
