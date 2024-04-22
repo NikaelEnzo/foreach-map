d2ab317bfe3c7b2907c00e35454a80117a118897
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 16:19:49 -0400

    Questão 15 concluida

---
+++ Questao14.js
@@ -0,0 +1,8 @@
+/* 
+Questão 14 - É possível encadear map() e forEach() juntos?
+
+sim, no codigo iriamos colocar em prioridade o map assim criando um novo array, depois usariamos forEach para alterar os valores do array gerado pelo map
+
+
+
+*/
