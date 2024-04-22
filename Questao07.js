525376a5229f63fc5409a7ca110dd86e5413d1f0
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-22 14:22:27 -0400

    Questão 07 concluida

---
+++ Questao07.js
@@ -0,0 +1,13 @@
+/* 
+Questão 07 – Crie um algoritmo usando o método map() para calcular o 
+quadrado de cada número em um array?
+*/
+
+let array = [1,2,3,4,5];
+
+let arrayNum = array.map(function(num){
+      
+    return num * num
+
+})
+console.log(arrayNum)
